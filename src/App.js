8ab91f6075507dec7b772db9c284b4710ecb74d9
Nikhil Kumar <unikhil723@gmail.com>
2026-04-14 14:05:43 +0530

    modift app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Login';
 import Register from './Register';
+import Home from './Home';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path='/Home' element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
