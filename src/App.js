6c7e01419242b2f8ea6557693ebd51e410dc893f
Nikhil Kumar <unikhil723@gmail.com>
2026-04-26 00:06:09 +0530

    add new file checker

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './Pages/Home/Home';
 import CreatePost from './createPost/CreatePost';
 import Profile from './Pages/Profile/Profile';
 import BottomNav from './components/BottomNav';
+import Leaderboard from './Pages/Leaderboard/Leaderboard';
 
 function Layout() {
   const location = useLocation();
@@ -19,6 +20,7 @@ function Layout() {
         <Route path="/home" element={<Home />} />
         <Route path="/create-post" element={<CreatePost />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
       </Routes>
       {!hideNav.includes(location.pathname) && <BottomNav />}
     </>
