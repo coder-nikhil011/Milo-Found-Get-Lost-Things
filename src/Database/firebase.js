fd00e3e139b38693315708dd1a01ebff1311a057
Nikhil Kumar <unikhil723@gmail.com>
2026-04-26 23:29:15 +0530

    final

--- src/Database/firebase.js
+++ src/Database/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { initializeFirestore, persistentLocalCache } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
@@ -14,11 +14,11 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
+export const db = initializeFirestore(app, {
+  localCache: persistentLocalCache()
+});
+
 export const auth = getAuth(app);
 export const storage = getStorage(app);
-enableIndexedDbPersistence(db).catch((err) => {
-  console.log('Persistence error:', err.code);
-});
 
 export default app;
